b5824f0680211afa289d9f900e1899e13ba1eb2f
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-04 15:34:30 -0600

    Clean up EX 1, EX 2, and EX 3

--- cleanUp.js
+++ cleanUp.js
@@ -3,44 +3,46 @@
 // YOUR TASK:
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
-  // make sure the code you submit WORKS - you can run this entire file 
+  // make sure the code you submit WORKS - you can run this entire file
     // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name");
 }
 
-nameQuestion()
+nameQuestion();
 
+// I added a semicolon the the end of line two and line 15 because that is following best practices and other coding languages may require it.
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
+var sum = first + second + third;
 console.log(sum);
-      }
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
-
+// I added a semicolon to line 21 and unindented the curly bracket on line 23.
 
 // EX 3:
-func makeFreshPesto(){
+func makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
+//Added a space between () and { on line 30. Moved } from line 35 to line 36.
 
-
-//  EX 4:
-function average(num1, num2) 
+/* //  EX 4:
+function average(num1, num2)
   {
 var sum = num1 + num2;
     var avg = sum / 2;
 
   console.log(`the average is: ${avg}`.)
-  }
+} */
