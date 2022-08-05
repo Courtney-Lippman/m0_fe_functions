0359f7bd4c738b4232de3d6d7076b254bd09e7aa
Courtney Lippman <courtney.lippman@colorado.edu>
2022-08-04 18:09:45 -0600

    Add two alternate ways to solve 4

--- functions.js
+++ functions.js
@@ -12,7 +12,7 @@ function onlyYes(yes) {
   console.log(`I only accept ${yes} as an answer!`);
 }
 
-onlyYes("YES");
+onlyYes("Yes");
 
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
@@ -57,4 +57,73 @@ checkStock(0, "Cheese");
 // => "Cheese - OUT of stock!"
 
 checkStock(1, "Salsa");
+// => "Salsa - running LOW"
+
+//4.5: I think I can make 4 more efficient:
+// write a function that checks the stock of items in a grocery store.
+// the function should be able to take any item and amount of that item as input
+// if there are 0 of the item, the function should tell us we are out of stock of that item
+// 1-3 and we are running LOW
+// 4 or more and we are overstocked
+
+//the following are examples of inputs and outputs for this function:
+
+function checkStocks(amount, item) {
+   if (amount >= 4) {
+     console.log(`${item} is stocked`);
+
+   } else if (amount > 0) {
+      console.log(`${item} is running LOW`);
+   } else if (amount === 0) {
+     console.log(`${item} is OUT of stock!`);
+   } else {
+     console.log("Error")
+   }
+
+
+}
+
+checkStocks(4, "Coffee");
+// => "Coffee is stocked"
+
+checkStocks(5, "Coffee");
+// => "Coffee is stocked"
+
+checkStocks(3, "Tortillas");
+// => "Tortillas - running LOW"
+
+checkStocks(0, "Cheese");
+// => "Cheese - OUT of stock!"
+
+checkStocks(1, "Salsa");
+// => "Salsa - running LOW"
+console.log("***********")
+function checkStocks2(amount, item) {
+   var stockStatus = ""
+   if (amount >= 4) {
+      stockStatus = " is stocked"
+
+   } else if (amount > 0) {
+     stockStatus = " - running low"
+
+   } else if (amount === 0) {
+    stockStatus = " - OUT of stock!"
+   }
+
+   console.log(item + stockStatus)
+
+}
+
+checkStocks2(4, "Coffee");
+// => "Coffee is stocked"
+checkStocks2(5, "Coffee");
+// => "Coffee is stocked"
+
+checkStocks2(3, "Tortillas");
+// => "Tortillas - running LOW"
+
+checkStocks2(0, "Cheese");
+// => "Cheese - OUT of stock!"
+
+checkStocks2(1, "Salsa");
 // => "Salsa - running LOW"
